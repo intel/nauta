d92472bb227e4ae5d528c3882306e91e04fe08e6
Adam Janikowski <adam.janikowski@intel.com>
2018-05-23 09:20:49 +0200

    CAN-346 remanage samba resources (#137)

--- dls4e-containers/samba/samba-create-user.sh
+++ dls4e-containers/samba/samba-create-user.sh
@@ -9,9 +9,18 @@ function set_object_int_value() {
     kubectl patch u $1 --type merge --patch "{\"spec\":{\"$2\":$3}}"
 }
 
+BLOCKED_USERNAMES="root admin centos samba"
+function username_blocked() {
+  for name in ${BLOCKED_USERNAMES}; do
+    if [ X"$1" == X"${name}" ]; then
+      return 1
+    fi
+  done
+}
+
 function get_free_uid() {
   if id $1 > /dev/null 2>&1; then
-      id -u user_$1
+      id -u $1
       return
   fi
   for n in {10000..20000}; do
@@ -23,7 +32,7 @@ function get_free_uid() {
 }
 
 function create_user() {
-  USER=user_$1
+  USER=$1
   kUID=$2
   PASSWORD=$3
   if ! id -u $kUID; then
@@ -36,16 +45,26 @@ function create_user() {
 }
 
 user=$1
+#if username_blocked ${user}; then
+#  echo "User name ${name} is blocked"
+#  exit 1
+#fi
 
 STATE=`kubectl get u $user -o 'jsonpath={.spec.state}'`
 echo "User $user in state $STATE"
 echo "Creating user $user"
-PASSWORD=`kubectl get u $user -o 'jsonpath={.spec.password}'`
-if [ X"$PASSWORD" == X"" ]; then
-    echo "Generating password"
-    PASSWORD=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1`
-    set_object_value $user password $PASSWORD
+if ! kubectl -n $user get secret password; then
+echo """
+apiVersion: v1
+kind: Secret
+metadata:
+  name: password
+  namespace: $user
+data:
+  password: `cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1 | base64 -w0`
+""" | kubectl create -f -
 fi
+PASSWORD=`kubectl -n $user get secret password -o 'jsonpath={.data.password}' | base64 -d`
 kUID=`kubectl get u $user -o 'jsonpath={.spec.uid}'`
 if [ X"$kUID" == X"" ]; then
     echo "Generating UID"
@@ -58,9 +77,6 @@ mkdir -p /input/$user
 mkdir -p /output/$user
 
 chown -R $kUID:$kUID /input/$user /output/$user
-echo "Create links"
-ln -sfn /input/$user /home/user_$user/input
-ln -sfn /output/$user /home/user_$user/output
 
 if [ X"$STATE" != X"CREATED" ]; then
     echo "Update user state"
