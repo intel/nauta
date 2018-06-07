a7b675e132d5549d5e434d815a6553ca78c43f90
jcchr <31657550+jcchr@users.noreply.github.com>
2018-06-07 14:52:23 +0200

    CAN-402 code coverage (#190)

---
+++ applications/experiment-service/code_coverage.sh
@@ -0,0 +1,20 @@
+min_code_coverage=80
+
+go test -coverprofile=cmd_cov.dat ./cmd/... >> /dev/null
+cmd_coverage=`go tool cover -func=cmd_cov.dat | grep total | awk -F' '  '{print $NF}'`
+rm cmd_cov.dat
+
+go test -coverprofile=pkg_cov.dat ./pkg/... >> /dev/null
+pkg_coverage=`go tool cover -func=pkg_cov.dat | grep total | awk -F' '  '{print $NF}'`
+rm pkg_cov.dat
+
+echo 'Code coverage:'
+echo 'cmd module : ' $cmd_coverage
+echo 'pkg module : ' $pkg_coverage
+
+if (( $(awk 'BEGIN {print ("'$cmd_coverage'" < "'$min_code_coverage'")}') || 
+      $(awk 'BEGIN {print ("'$pkg_coverage'" < "'$min_code_coverage'")}') )); then
+  echo 'Code coverage is too low'
+  exit 1
+fi
+
