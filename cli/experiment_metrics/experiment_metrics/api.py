95301aca23fbf6b038733a37890e5a742350370a
jwierzbo <jakub.a.wierzbowski@intel.com>
2018-08-07 10:30:21 +0200

    CAN-893 Remove info logs from metrics libs

--- cli/experiment_metrics/experiment_metrics/api.py
+++ cli/experiment_metrics/experiment_metrics/api.py
@@ -60,9 +60,8 @@ def publish(metrics):
 
     for i in range(MAX_RETRIES_COUNT):
         try:
-            raw_run = api.patch_namespaced_custom_object(group='aggregator.aipg.intel.com', namespace=namespace,
-                                                         body=body, plural=RUN_PLURAL, version=RUN_VERSION, name=name)
-            logger.info('Run patch response : {}'.format(raw_run))
+            api.patch_namespaced_custom_object(group='aggregator.aipg.intel.com', namespace=namespace, body=body,
+                                               plural=RUN_PLURAL, version=RUN_VERSION, name=name)
             break
         except ApiException as e:
             if e.status != HTTPStatus.CONFLICT or i == MAX_RETRIES_COUNT-1:
