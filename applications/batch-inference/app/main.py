9c823ec3edbab543d4d1506d91022702148cc70b
Piotr Skindel <piotr.skindel@intel.com>
2018-09-12 20:02:29 +0200

    CAN-1055: batch inference - resuming (#524)

--- applications/batch-inference/app/main.py
+++ applications/batch-inference/app/main.py
@@ -20,38 +20,68 @@
 #
 
 import argparse
+import logging
 import os
 from time import sleep
 from threading import Thread
+from typing import Optional
 
-import grpc
-
-from tensorflow_serving.apis import predict_pb2, prediction_service_pb2_grpc
 from experiment_metrics.api import publish
+import grpc
+from kubernetes import config, client
+from tensorflow_serving.apis import predict_pb2, prediction_service_pb2_grpc
+
+PROGRESS_METRIC_KEY = 'progress'
+
+API_GROUP_NAME = 'aggregator.aipg.intel.com'
+RUN_PLURAL = 'runs'
+RUN_VERSION = 'v1'
 
 progress = 0
 max_progress = 1
 stop_thread = False
 
-if not os.getenv('RUN_NAME', None):
-    raise RuntimeError('RUN_NAME env var must be set for publishing progress metrics!')
+log_level_env_var = os.getenv('LOG_LEVEL')
+
+if log_level_env_var:
+    desired_log_level = logging.getLevelName(log_level_env_var.upper())
+    if desired_log_level not in (logging.CRITICAL, logging.ERROR, logging.WARNING, logging.INFO, logging.DEBUG):
+        desired_log_level = logging.INFO
+else:
+    desired_log_level = logging.INFO
+
+logging.basicConfig(level=desired_log_level)
 
 
-def do_batch_inference(server_address, input_dir_path, output_dir_path):
+def do_batch_inference(server_address: str, input_dir_path: str, output_dir_path: str, related_run_name: str):
     detected_files = []
 
     for root, _, files in os.walk(input_dir_path):
         for name in files:
             detected_files.append(os.path.join(root, name))
 
-    global max_progress
-    max_progress = len(detected_files)
+    detected_files.sort()
 
     channel = grpc.insecure_channel(server_address)
     stub = prediction_service_pb2_grpc.PredictionServiceStub(channel)
 
-    for i, data_file in enumerate(detected_files, start=1):
+    global max_progress
+    global progress
+    max_progress = len(detected_files)
+
+    reverted_progress = try_revert_progress(related_run_name)
+    if reverted_progress:
+        logging.debug(f"new progress for processing: {progress}")
+        progress = reverted_progress
+    else:
+        logging.debug("no progress reverted")
+
+
+    files_to_process = detected_files[progress:]
+
+    for data_file in files_to_process:
         request = predict_pb2.PredictRequest()
+        logging.debug(f"processing file: {data_file}")
         with open(data_file, mode='rb') as fi:
             pb_bytes = fi.read()
 
@@ -64,31 +94,71 @@ def do_batch_inference(server_address, input_dir_path, output_dir_path):
 
         result_pb_serialized: bytes = result.SerializeToString()
 
-        with open(f'{output_dir_path}/{os.path.basename(data_file)}.pb', mode='wb') as fi:
+        with open(f'{output_dir_path}/{os.path.basename(data_file)}', mode='wb') as fi:
             fi.write(result_pb_serialized)
 
-        global progress
-        progress = i
-        print(f'progress: {progress}/{max_progress}')
+        progress += 1
+        logging.info(f'progress: {progress}/{max_progress}')
 
 
 def publish_progress():
+    logging.debug("starting publish_progress ...")
     progress_percent = 0
     while progress_percent != 100 and not stop_thread:
         new_progress_percent = progress/max_progress * 100
-
+        logging.debug(f"new_progress_percent: %.1f" % new_progress_percent)
         if new_progress_percent != progress_percent:
             progress_percent = new_progress_percent
 
             metrics = {
-                'progress': str(progress_percent)
+                PROGRESS_METRIC_KEY: str("%.1f" % progress_percent)
             }
+            logging.debug("publishing metrics ...")
             publish(metrics)
 
         sleep(1)
 
 
+def try_revert_progress(run_name: str) -> Optional[int]:
+    logging.debug("trying to revert progress...")
+    config.load_incluster_config()
+
+    with open("/var/run/secrets/kubernetes.io/serviceaccount/namespace", mode='r') as file:
+        my_current_namespace = file.read()
+
+    if not my_current_namespace:
+        raise RuntimeError(f"error reading my current namespace {str(my_current_namespace)}")
+
+    runs_custom_obj_client = client.CustomObjectsApi()
+
+    try:
+        run = runs_custom_obj_client.get_namespaced_custom_object(group=API_GROUP_NAME, version=RUN_VERSION,
+                                                                  plural=RUN_PLURAL, namespace=my_current_namespace,
+                                                                  name=run_name)
+    except Exception:
+        logging.exception("error when contacting to kubernetes API")
+        return None
+
+    try:
+        saved_progress: str = run['spec']['metrics']['progress']
+    except KeyError:
+        logging.debug(f"no progress metric detected")
+        return None
+
+    logging.debug(f"progress reverted! progress from metrics: {saved_progress}")
+    saved_progress: float = float(saved_progress)
+
+    real_progress = saved_progress/100 * max_progress
+
+    progress = int(real_progress)
+    return progress
+    
+
 def main():
+    related_run_name = os.getenv('RUN_NAME')
+    if not related_run_name:
+        raise RuntimeError('RUN_NAME env var must be set for publishing progress metrics!')
+
     parser = argparse.ArgumentParser()
     parser.add_argument('--input_dir_path', type=str)
     parser.add_argument('--output_dir_path', type=str)
@@ -107,7 +177,8 @@ def main():
     try:
         do_batch_inference(server_address=os.getenv('TENSORFLOW_MODEL_SERVER_SVC_NAME', ''),
                            input_dir_path=input_dir_path,
-                           output_dir_path=output_dir_path)
+                           output_dir_path=output_dir_path,
+                           related_run_name=related_run_name)
     except Exception:
         global stop_thread
         stop_thread = True
