c7abed58900e6ec3597dab10e2f2adb846342c39
janekmichalik <37070851+janekmichalik@users.noreply.github.com>
2019-03-12 11:09:22 +0100

    CAN-1932_alexnet_script (#1269)
    * CAN-1932_alexnet_script
    
    * CAN-1932_alexnet_script
    
    * CAN-1932_alexnet_script

--- applications/cli/example-python/package_examples/alexnet_single_node.py
+++ applications/cli/example-python/package_examples/alexnet_single_node.py
@@ -1,22 +1,6 @@
-#
-# Copyright (c) 2019 Intel Corporation
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
 # BSD 3-Clause License
 
-# Copyright (c) 2017, Frederik Kratzert
+# Copyright (c) 2019, Frederik Kratzert. Intel Corporation.
 # All rights reserved.
 
 # Redistribution and use in source and binary forms, with or without
@@ -43,50 +27,60 @@
 # CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
+import argparse
 import os
 import sys
 import time
 
 import tensorflow as tf
+import tensorflow.contrib.keras as keras
 
-from imagenet_loader_keras import create_generator
 from alexnet_model import AlexNet
 
 
-tf.app.flags.DEFINE_integer('training_epoch', 1,
-                            'number of training epochs.')
-tf.app.flags.DEFINE_integer('model_version', 1, 'version number of the model.')
-tf.app.flags.DEFINE_integer('batch_size', 128, 'size of image batches.')
-tf.app.flags.DEFINE_string('data_dir', './dataset/imagenet', 'training data directory.')
-tf.app.flags.DEFINE_string('output_dir', './output', 'output directory for model saving.')
-FLAGS = tf.app.flags.FLAGS
+# version number of the model
+MODEL_VERSION = 1
+
+FLAGS = None
+
+
+def create_generator(path_train, path_val, batch_size=128):
+    datagen = keras.preprocessing.image.ImageDataGenerator(data_format='channels_last')
+    train_generator = datagen.flow_from_directory(path_train,
+                                                  target_size=(227, 227),
+                                                  batch_size=batch_size,
+                                                  class_mode='categorical')
+
+    validation_generator = datagen.flow_from_directory(path_val,
+                                                       target_size=(227, 227),
+                                                       batch_size=batch_size,
+                                                       class_mode='categorical')
+    return train_generator, validation_generator
 
 
 def main(_):
     # here we train and validate the model
     if len(sys.argv) < 2:
         print('Example usage: alexnet.py [--training_epoch=20] '
-              '[--model_version=4] [--data_dir=/data/dir] [--output_dir=/output/dir]')
+              '[--data_dir=/data/dir] [--export_dir=/output/dir]')
         sys.exit(-1)
+
     if FLAGS.training_epoch <= 0:
         print('Please specify a positive value for training iteration.')
         sys.exit(-1)
-    if FLAGS.model_version <= 0:
-        print('Please specify a positive value for version number.')
-        sys.exit(-1)
+
     if FLAGS.batch_size <= 0:
         print('Please specify a positive value for batch size.')
         sys.exit(-1)
 
     batch_size = FLAGS.batch_size
 
-    print('Loading data')
+    print('Loading data...')
     training, testing = create_generator(os.path.join(FLAGS.data_dir, 'i1k-extracted/train'),
                                          os.path.join(FLAGS.data_dir, 'i1k-extracted/val'),
                                          batch_size=batch_size)
 
-    print('Data loaded.')
+    print('Data loaded!')
 
     display_step = 20
     train_size = training.samples
@@ -100,31 +94,29 @@ def main(_):
     x_3d = tf.reshape(x_flat, shape=(tf.shape(x_flat)[0], image_size,
                                      image_size, img_channel))
     y = tf.placeholder(tf.float32, [None, n_classes])
+
     keep_prob = tf.placeholder(tf.float32)
 
     model = AlexNet(x_3d, keep_prob=keep_prob, num_classes=n_classes)
     model_train = model.fc8
-
     model_prediction = tf.nn.softmax(model_train)
 
     cost = tf.reduce_mean(
         tf.nn.softmax_cross_entropy_with_logits(logits=model_train, labels=y))
     global_step = tf.Variable(0, trainable=False, name='global_step')
 
-    lr = tf.train.exponential_decay(0.01, global_step, 100000, 0.1,
-                                    staircase=True)
+    lr = tf.train.exponential_decay(0.01, global_step, 100000, 0.1, staircase=True)
 
     optimizer = tf.train.MomentumOptimizer(learning_rate=lr, momentum=0.9).minimize(cost, global_step=global_step)
 
-    accuracy, update_op = tf.metrics.accuracy(labels=tf.argmax(y, 1),
-                                              predictions=tf.argmax(model_prediction,
-                                                                    1))
+    accuracy, update_op = tf.metrics.accuracy(labels=tf.argmax(y, 1), predictions=tf.argmax(model_prediction, 1))
+
     test_accuracy, test_update_op = tf.metrics.accuracy(labels=tf.argmax(y, 1),
-                                                        predictions=tf.argmax(
-                                                            model_prediction, 1))
+                                                        predictions=tf.argmax(model_prediction, 1))
 
     start_time = time.time()
-    print("Start time is: " + str(start_time))
+    print("Start time is: {}".format(str(start_time)))
+
     with tf.Session() as sess:
         tf.global_variables_initializer().run()
         tf.local_variables_initializer().run()
@@ -145,11 +137,13 @@ def main(_):
                 print(" Iter " + str(step) + ", Minibatch Loss= " + "{:.6f}".format(loss) +
                       ", Training Accuracy= " + "{}".format(acc) + " Elapsed time:" + str(elapsed_time))
 
+        stop_time = time.time()
         print("Optimization Finished!")
-        print("Training took" + str(time.time() - start_time))
+        print("Training took: {}".format(stop_time-start_time))
 
         step_test = 1
         acc_list = []
+
         while step_test * batch_size < testing.samples:
             testing_xs, testing_ys = testing.next()
             acc_up = sess.run([test_accuracy, test_update_op],
@@ -160,10 +154,11 @@ def main(_):
             step_test += 1
 
         # save model using SavedModelBuilder from TF
-        export_path_base = FLAGS.output_dir
+        export_path_base = FLAGS.export_dir
         export_path = os.path.join(
             tf.compat.as_bytes(export_path_base),
-            tf.compat.as_bytes(str(FLAGS.model_version)))
+            tf.compat.as_bytes(str(MODEL_VERSION)))
+
         print('Exporting trained model to', export_path)
         builder = tf.saved_model.builder.SavedModelBuilder(export_path)
 
@@ -191,8 +186,22 @@ def main(_):
         print('Done exporting!')
         print("Max batch accuracy is", max(acc_list))
         print("Min batch accuracy is", min(acc_list))
-        print("Validation accuracy:", sum(acc_list) / len(acc_list))
+        print("Avg. accuracy:", sum(acc_list) / len(acc_list))
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data_dir", type=str,
+                        default="./dataset/imagenet",
+                        help="Directory which contains dataset")
+    parser.add_argument("--training_epoch", type=int,
+                        default=1,
+                        help="Number of training epochs")
+    parser.add_argument("--batch_size", type=int,
+                        default=128,
+                        help="Size of image batches")
+    parser.add_argument("--export_dir", type=str,
+                        default="./output",
+                        help="Export directory for model")
+    FLAGS, _ = parser.parse_known_args()
     tf.app.run()
