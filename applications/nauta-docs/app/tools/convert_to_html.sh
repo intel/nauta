bc46870c4d0418347c80bf76bd143bcc821dd145
Michał Żyłowski <michal.zylowski@intel.com>
2019-03-25 16:01:12 +0100

    CAN-2179: Introduce application for md files conversion (#1395)

---
+++ applications/nauta-docs/app/tools/convert_to_html.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2019 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+set -x
+
+cd user-guide
+find . -name "*.md*" | while read file; do \
+    pandoc -f markdown \
+    --template=/app/tools/template.html \
+    --toc --toc-depth=3 --standalone \
+    --lua-filter=/app/tools/links.lua \
+    --metadata version="$1" \
+    --metadata title="Nauta documentation: ${file%.*}.html" \
+    "$file" -o "${file%.*}.html"; done
