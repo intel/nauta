e678d9589b4c3a54ce3d51f390bcdcd3ee8c3ecb
Mateusz Ciesielski <mateusz.j.ciesielski@intel.com>
2019-04-11 13:57:55 +0200

    Postgres image build (#1517)

--- nauta-charts/gitea/templates/postgres/_container.tpl
+++ nauta-charts/gitea/templates/postgres/_container.tpl
@@ -21,7 +21,7 @@ Create helm partial for postgres server
   - name: POSTGRES_INITDB_ARGS
     value: {{ default "" .Values.inPodPostgres.postgresInitdbArgs | quote }}
   - name: PGDATA
-    value: /var/lib/postgresql/data/pgdata
+    value: /var/lib/pgsql/data
   - name: POSTGRES_PASSWORD
     valueFrom:
       secretKeyRef:
@@ -32,24 +32,6 @@ Create helm partial for postgres server
   ports:
   - name: postgresql
     containerPort: 5432
-  livenessProbe:
-    exec:
-      command:
-      - sh
-      - -c
-      - exec pg_isready --host $POD_IP
-    initialDelaySeconds: 120
-    timeoutSeconds: 5
-    failureThreshold: 6
-  readinessProbe:
-    exec:
-      command:
-      - sh
-      - -c
-      - exec pg_isready --host $POD_IP
-    initialDelaySeconds: 5
-    timeoutSeconds: 3
-    periodSeconds: 5
   resources:
 {{ toYaml .Values.resources.postgres | indent 10 }}
   volumeMounts:
---
+++ nauta-containers/postgres/postgres.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -e
+#
+# Copyright (c) 2019 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+echo "Checking PGDATA($PGDATA) content..."
+ls -A "${PGDATA}"
+
+if [ -z "$(ls -A ${PGDATA})" ]; then
+    echo 'Initialize postgres...'
+    chown -R postgres:postgres "${PGDATA}"
+    if [ -n "${POSTGRES_PASSWORD}" ]; then
+        echo 'Setting up postgres with password..'
+        echo "${POSTGRES_PASSWORD}" > /tmp/pwfile
+        PGSETUP_INITDB_OPTIONS='--pwfile=/tmp/pwfile' postgresql-setup initdb 
+    else
+        postgresql-setup initdb
+    fi
+    cat /var/lib/pgsql/initdb.log || true
+
+    cp /etc/pg_hba.conf "${PGDATA}/pg_hba.conf"
+    cp /etc/postgresql.conf "${PGDATA}/postgresql.conf"
+
+    if [ -n "${POSTGRES_DB}" ]; then
+        echo "POSTGRES_DB: ${POSTGRES_DB}"
+        su - postgres -c "echo CREATE DATABASE ${POSTGRES_DB}\; | PGDATA=${PGDATA} postgres --single"
+    fi
+fi
+
+su - postgres -c "PGDATA=${PGDATA} postgres"
